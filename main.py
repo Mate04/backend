61e3f14fc7ebd3e83794a1d3d798254c4f5c7887
Mate04 <matusmaldonado@hotmail.com>
2024-03-08 15:43:13 -0300

    habilitacion de cors

--- main.py
+++ main.py
@@ -2,14 +2,30 @@ from fastapi import FastAPI
 from dataBot import data
 from models.structureMsg import strucMsg
 from response import response
+from dotenv import load_dotenv
+import os
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+load_dotenv()
+URLAuth = os.getenv('URLAUTHENTIC')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
 
 @app.get('/')
 def root():
-    return 
+    return {'url_athentic':URLAuth}
 @app.post('/')
 def root(strucMsg:strucMsg):
-    return response(strucMsg)
+    print(strucMsg)
+    data = response(strucMsg)
+    print(data)
+    return data
 @app.get('/getData')
 def dataBot():
     return data()
